5510efab8f37c3bf212ce7598a2d15bf31ec6a34
Gochad <malaga.drag@gmail.com>
2024-12-15 21:55:22 +0100

    collect lidar data and pull example data from the game

--- mod/DataCollector.cs
+++ mod/DataCollector.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using GTA;
 using GTA.Math;
+using GTA.Native;
 
 namespace DataCollectorNamespace
 {
@@ -25,6 +26,72 @@ namespace DataCollectorNamespace
 
                 string dataLine = $"{DateTime.Now},{position.X},{position.Y},{position.Z},{speed},{heading}";
                 dataWriter.WriteLine(dataLine);
+            }
+        }
+
+        public void Close()
+        {
+            if (dataWriter != null)
+            {
+                dataWriter.Close();
+                dataWriter = null;
+            }
+        }
+    }
+    
+    public class LidarCollector
+    {
+        private StreamWriter dataWriter;
+        private int raysPer360 = 360;
+        private float maxDistance = 100f;
+        private Vector3 sensorOffset = new Vector3(0, 0, 1);
+
+        public LidarCollector(string filePath)
+        {
+            dataWriter = new StreamWriter(filePath, false);
+            dataWriter.WriteLine("Time,SensorPosX,SensorPosY,SensorPosZ,Angle,HitDistance,HitEntity");
+        }
+
+        public void CollectLidarData(Vehicle vehicle)
+        {
+            if (vehicle != null && vehicle.Exists())
+            {
+                Vector3 sensorPosition = vehicle.Position + sensorOffset;
+                DateTime now = DateTime.Now;
+
+                for (int i = 0; i < raysPer360; i++)
+                {
+                    float angleDeg = i;
+                    float angleRad = angleDeg * (float)(Math.PI / 180.0);
+                    Vector3 direction = new Vector3((float)Math.Cos(angleRad), (float)Math.Sin(angleRad), 0f);
+
+                    RaycastResult result = World.Raycast(sensorPosition, sensorPosition + direction * maxDistance, IntersectFlags.Everything);
+                    
+                    float hitDistance = maxDistance;
+                    string hitEntity = "None";
+
+                    if (result.DidHit)
+                    {
+                        hitDistance = sensorPosition.DistanceTo(result.HitPosition);
+
+                        if (result.HitEntity != null)
+                        {
+                            if (result.HitEntity is Ped)
+                                hitEntity = "Ped";
+                            else if (result.HitEntity is Vehicle)
+                                hitEntity = "Vehicle";
+                            else
+                                hitEntity = "Object";
+                        }
+                        else
+                        {
+                            hitEntity = "WorldCollision";
+                        }
+                    }
+
+                    string line = $"{now},{sensorPosition.X},{sensorPosition.Y},{sensorPosition.Z},{angleDeg},{hitDistance},{hitEntity}";
+                    dataWriter.WriteLine(line);
+                }
             }
         }
 
--- mod/Main.cs
+++ mod/Main.cs
@@ -12,6 +12,7 @@ namespace template
         private bool hasEnteredVehicle = false;
         private Vehicle currentVehicle;
         private DataCollector dataCollector;
+        private LidarCollector lidarCollector;
 
         public Main()
         {
@@ -22,6 +23,11 @@ namespace template
             
             string filePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "GTA_Data.csv");
             dataCollector = new DataCollector(filePath);
+            
+            string lidarFilePath = System.IO.Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), 
+                "GTA_LidarData.csv");
+            lidarCollector = new LidarCollector(lidarFilePath);
         }
 
         private void OnTick(object sender, EventArgs e)
@@ -39,24 +45,30 @@ namespace template
             if (hasEnteredVehicle && currentVehicle != null)
             {
                 dataCollector.CollectData(currentVehicle);
+                lidarCollector.CollectLidarData(currentVehicle);
             }
         }
 
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.F7)
+            switch (e.KeyCode)
             {
-                WaypointManager.SetWaypoint(-1034.6f, -2733.6f);
-            }
-            
-            if (e.KeyCode == Keys.F8)
-            {
-                ObstacleManager.AddObstacle();
-            }
-            
-            if (e.KeyCode == Keys.F9)
-            {
-                VehicleManager.EnterNearestVehicleAndDrive();
+                case Keys.F7:
+                    WaypointManager.SetWaypoint(-1034.6f, -2733.6f);
+                    break;
+
+                case Keys.F8:
+                    ObstacleManager.AddObstacle();
+                    break;
+
+                case Keys.F9:
+                    VehicleManager.EnterNearestVehicleAndDrive();
+                    break;
+
+                case Keys.F10:
+                    dataCollector.Close();
+                    lidarCollector.Close();
+                    break;
             }
         }
     }
