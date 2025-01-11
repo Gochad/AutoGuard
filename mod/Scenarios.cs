3424790d248cb921e89e0e1968db5e282fc980ba
Gochad <malgorzata.drag@spoton.com>
2025-01-11 20:35:40 +0100

    move scenario

--- mod/Scenarios.cs
+++ mod/Scenarios.cs
@@ -26,15 +26,7 @@ namespace drivingMod
             ));
 
             list.Add(new TestScenario(
-                name: "Test3_highway_speed",
-                start: new Vector3(857.6f, 111.4f, 70),
-                heading: 270.0f,
-                waypointPos: new Vector3(2063.8f, 1435.2f, 75.1f),
-                spawnObstacle: false
-            ));
-
-            list.Add(new TestScenario(
-                name: "Test4_train_crossing",
+                name: "Test3_train_crossing",
                 start: new Vector3(367.3f, -581.4f, 28.7f),
                 heading: 90.0f,
                 waypointPos: new Vector3(799.2f, -1485.7f, 27),
@@ -42,10 +34,18 @@ namespace drivingMod
             ));
 
             list.Add(new TestScenario(
-                name: "Test5_mountain_road",
+                name: "Test4_mountain_road",
                 start: new Vector3(1387, -1615.1f, 55.6f),
                 heading: 45.0f,
                 waypointPos: new Vector3(1729.1f, -1582.9f, 111.8f),
+                spawnObstacle: false
+            ));
+
+            list.Add(new TestScenario(
+                name: "Test5_highway_speed",
+                start: new Vector3(857.6f, 111.4f, 70),
+                heading: 270.0f,
+                waypointPos: new Vector3(2063.8f, 1435.2f, 75.1f),
                 spawnObstacle: false
             ));
             
